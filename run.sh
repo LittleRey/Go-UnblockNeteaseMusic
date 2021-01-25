44f59645993a866e1e37d813f733f691073b1e44
LittleRey <34397448+LittleRey@users.noreply.github.com>
2021-01-25 14:51:22 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-UnblockNeteaseMusic -p 8080 -sp 8443 -c /server.crt -k /server.key -m 0 -o kuwo:kugou:migu -sl 3 -b
+./UnblockNeteaseMusic -p 8080 -sp 8443 -c /server.crt -k /server.key -m 0 -o kuwo:kugou:migu -sl 3 -b
