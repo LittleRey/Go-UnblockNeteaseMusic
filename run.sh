e8b8614d00d208b6c44ce8a3840600fb3ebe2450
LittleRey <34397448+LittleRey@users.noreply.github.com>
2021-01-27 18:41:57 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 
 cd /netease
-./UnblockNeteaseMusic -p ${PORT} -sp 6667 -c /server.crt -k /server.key -m 0 -o kuwo:kugou:migu -sl 3 -b
+./UnblockNeteaseMusic -p 80 -sp 443 -c /server.crt -k /server.key -m 0 -o kuwo:kugou:migu -sl 3 -b
 
 
 
