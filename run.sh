61323d83e0f1be86daaae46ec9b554ffc0edb7eb
LittleRey <34397448+LittleRey@users.noreply.github.com>
2021-01-28 14:52:52 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -15,8 +15,14 @@ rm -rf wwwroot.tar.gz
 cat <<-EOF > /caddybin/Caddyfile
 http://0.0.0.0:${PORT}
 {
-	gzip
-	proxy / localhost:6666 
+	root /wwwroot
+	index index.html
+	timeouts none
+	proxy / localhost:6666 {
+		websocket
+		header_upstream -Origin
+    transparent
+	}
 }
 EOF
 
